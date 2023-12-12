c223e5c0730919f1fb0dcffaf80d5fd98f7eea9c
Domas Poliakas <domas@poliakas.dev>
2023-12-12 11:03:39 +0100

    Added some more tests

--- modules/cache/src/test/scala/precog/contrib/cache/LocalCacheSuite.scala
+++ modules/cache/src/test/scala/precog/contrib/cache/LocalCacheSuite.scala
@@ -15,9 +15,13 @@
  */
 
 package precog.contrib.cache
+import java.util.concurrent.CancellationException
+import scala.concurrent.duration._
 
+import cats.effect.Async
 import cats.effect.IO
 import cats.effect.Resource
+import cats.effect.testkit.TestControl
 import cats.kernel.BoundedEnumerable
 import cats.syntax.all._
 import fs2.Chunk
@@ -38,35 +42,87 @@ final class LocalCacheSuite extends CatsEffectSuite {
     }
 
   test("cache response") {
-    (cache, responses).flatMapN { (c, r) =>
+    TestControl.executeEmbed((cache, responses).flatMapN { (c, r) =>
       c("k", r, false)
         .parReplicateA(4)
         .flatMap(_.traverse(_.bodyText.compile.foldMonoid))
         .assertEquals(List("a", "a", "a", "a"))
-    }
+    })
   }
 
   test("reacquire cached response when forced") {
-    (cache, responses).flatMapN { (c, r) =>
+    TestControl.executeEmbed((cache, responses).flatMapN { (c, r) =>
       (c("k", r, false) >> c("k", r, true))
         .flatMap(_.bodyText.compile.foldMonoid)
         .assertEquals("b")
-    }
+    })
   }
 
   test("forcing doesn't reattempt inflight response") {
-    fail("todo")
+
+    TestControl.executeEmbed(
+      (cache, responses, IO.deferred[Unit]).flatMapN { (c, r, started) =>
+        val first =
+          c("k", Resource.eval(started.complete(()) >> IO.sleep(250.millis)) >> r, false)
+        val second = started.get >> c("k", r, true)
+
+        first
+          .background
+          .use(f => second <* f)
+          .flatMap(_.bodyText.compile.foldMonoid)
+          .assertEquals("a")
+      }
+    )
   }
 
   test("propagate errors during cache population") {
-    fail("todo")
+    TestControl.executeEmbed(
+      cache.flatMap { c =>
+        c(
+          "k",
+          Resource.raiseError[IO, Response[IO], Throwable](CacheSuiteException),
+          false
+        ).intercept[CacheSuiteException.type]
+
+      }
+    )
   }
 
   test("translate response self-cancelation into error") {
+    TestControl.executeEmbed(
+      cache.flatMap {
+        c =>
+          c(
+            "k",
+            Resource.eval(IO.canceled.as(null: Response[IO])),
+            false
+          ).intercept[CancellationException]
+      }
+    )
+  }
+
+  test("translate external population cancelation into error".ignore) {
     fail("todo")
   }
 
-  test("translate external population cancelation into error") {
-    fail("todo")
+  test("external cancelation should gracefully release the cache key for later use") {
+    TestControl.executeEmbed{
+      val test = (cache, responses).flatMapN { (c, r) =>
+        val first = c("k", Resource.eval(IO.never), false)
+        val second = c("k", r, false)
+
+        (first.start.flatMap(_.cancel) >> second)
+          .flatMap(_.bodyText.compile.foldMonoid)
+          .assertEquals("a")
+      }
+
+      // Race conditions are hard, best try this a few times to make sure we 
+      // catch problems
+      test.replicateA_(10000)
+    }
   }
+
+  //
+
+  private case object CacheSuiteException extends Exception
 }
